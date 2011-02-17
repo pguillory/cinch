f8696914df562215e88b75b2c23d1119ee0edea7
Preston Guillory <pguillory@gmail.com>
2011-02-17 13:27:08 -0600

    transform_streamlined_function_call_in_expression()

--- test/tests/1. Functions/7. Return expression containing streamlined function call/index.js
+++ test/tests/1. Functions/7. Return expression containing streamlined function call/index.js
@@ -6,7 +6,7 @@ exports.test = function(next, assert) {
     });
 };
 function sum_squares(a, b, callback) {
-    return square(a, function(err, result1) {
+    square(a, function(err, result1) {
         if (err) {
             if (callback) {
                 return callback(err)
@@ -14,7 +14,7 @@ function sum_squares(a, b, callback) {
                 throw err
             }
         };
-        return square(b, function(err, result2) {
+        square(b, function(err, result2) {
             if (err) {
                 if (callback) {
                     return callback(err)
