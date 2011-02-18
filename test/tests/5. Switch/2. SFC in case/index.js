c65e5ccd57ebc1dd1c21c76c2860e0515ab9adc5
Preston Guillory <pguillory@gmail.com>
2011-02-18 15:36:33 -0600

    Removed debug output.

--- test/tests/5. Switch/2. SFC in case/index.js
+++ test/tests/5. Switch/2. SFC in case/index.js
@@ -7,7 +7,6 @@ exports.test = function(next, assert) {
 };
 function f(__callback_2) {
     __callback_2 = (__callback_2 || __throw_1);
-    console.log("5.2. f_()");
     switch ("foo") {
       case "bar":
         return __case_3();
@@ -18,11 +17,9 @@ function f(__callback_2) {
     };
     __rest_6();
     function __case_3() {
-        console.log("5.2. case bar");
         __rest_6();
     };
     function __case_4() {
-        console.log("5.2. case foo");
         return g(function(err, __result_7) {
             if (err) {
                 return __callback_2(err)
@@ -31,17 +28,14 @@ function f(__callback_2) {
         });
     };
     function __case_5() {
-        console.log("5.2. default");
         return __callback_2(null, 7);
     };
     function __rest_6() {
-        console.log("5.2. rest");
         return __callback_2(null);
     };
 };
 function g(__callback_8) {
     __callback_8 = (__callback_8 || __throw_1);
-    console.log("5.2. g_()");
     return __callback_8(null, 5);
 };
 function __throw_1(err) {
