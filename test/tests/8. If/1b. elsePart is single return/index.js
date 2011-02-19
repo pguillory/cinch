d2086c5fde797ed205d8e455fb6d1b1f41f64627
Preston Guillory <pguillory@gmail.com>
2011-02-18 23:10:21 -0600

    Fixed 'if' transformation discarding unblocked statement as the thenPart/elsePart.

---
+++ test/tests/8. If/1b. elsePart is single return/index.js
@@ -0,0 +1,31 @@
+exports.test = function(next, assert) {
+    f(function(err, result) {
+        assert.equal(result, 5);
+        next();
+    });
+};
+var f = function(__callback_2) {
+    __callback_2 = (__callback_2 || __throw_1);
+    if (false) {
+        __then_block_3();
+    }
+     else {
+        __else_block_4();
+    }
+;
+    function __then_block_3() {
+        __rest_block_5();
+    };
+    function __else_block_4() {
+        return __callback_2(null, 5);
+    };
+    function __rest_block_5() {
+        return __callback_2(null);
+    };
+};
+function __throw_1(err) {
+    if (err) {
+        throw err;
+    }
+;
+};
