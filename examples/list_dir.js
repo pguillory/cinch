e36538c371b132d140eb3f3d278d71533509936c
Preston Guillory <pguillory@gmail.com>
2011-02-19 01:42:39 -0600

    Added a better example.  Added bin/node-cinch.

--- examples/list_dir.js
+++
@@ -1,8 +0,0 @@
-require('..').registerExtension({ saveSource: true });
-
-var path = require('path')
-var file_utils = require('./file_utils');
-
-var dir = path.join(process.cwd(), process.argv[2] || '.')
-
-file_utils.recursive_ls(dir, 2)
