8481e1d0db6075a81838c04cc3f1f0a00a173531
Preston Guillory <pguillory@gmail.com>
2011-02-22 14:29:55 -0600

    Added cinch.transformSource().

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 exports.registerExtension = require('./lib/register').registerExtension;
-exports.transform = require("./lib/transform").transform;
+exports.transformSource = require("./lib/transform").transformSource;
 exports.transformFile = require("./lib/transform").transformFile;
